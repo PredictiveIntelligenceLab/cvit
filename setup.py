1a60acc1d2267cd2f64ae4517a5cb802035bfe2c
sifanw <sifanw@sas.upenn.edu>
2024-05-23 13:10:14 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+_CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+try:
+    README = open(os.path.join(_CURRENT_DIR, "README.md"), encoding="utf-8").read()
+except IOError:
+    README = ""
+
+setup(
+    name="cvit",
+    version="",
+    url=" ",
+    author="",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires=">=3.8",
+    install_requires=required,
+    license="Apache 2.0",
+    description=" ",
+    long_description=open(os.path.join(_CURRENT_DIR, "README.md")).read(),
+    long_description_content_type="text/markdown",
+)
